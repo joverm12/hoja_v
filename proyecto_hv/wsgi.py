6c6da643de7d69631782ce2e5801a1eb30e325bb
joverm12 <joverjesusmoreiramero@gmail.com>
2026-01-28 18:00:40 -0500

    si se puede

--- proyecto_hv/wsgi.py
+++ proyecto_hv/wsgi.py
@@ -1,14 +1,13 @@
 import os
-import cloudinary
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise # <--- ESTO ES VITAL
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proyecto_hv.settings')
 
-cloudinary.config( 
-  cloud_name = os.environ.get('CLOUDINARY_CLOUD_NAME'), 
-  api_key = os.environ.get('CLOUDINARY_API_KEY'), 
-  api_secret = os.environ.get('CLOUDINARY_API_SECRET'),
-  secure = True
-)
-
+# 1. Iniciamos la aplicación normal
 application = get_wsgi_application()
+
+# 2. La envolvemos con WhiteNoise para que Render encuentre los estilos y el JS del formulario
+# Esto elimina el error "No hay directorio en: /static_final/"
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'static_final'))
