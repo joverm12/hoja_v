e0cd31d947de0675944b92daf896133b565b71a6
joverm12 <joverjesusmoreiramero@gmail.com>
2026-01-28 16:41:31 -0500

    se puede

--- proyecto_hv/settings.py
+++ proyecto_hv/settings.py
@@ -2,14 +2,16 @@ import os
 import dj_database_url
 from pathlib import Path
 
+# --- CONFIGURACIÓN BÁSICA ---
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-abkt2ns#zz0)%=*^%7-n5oihoq+h754!b=g1#fl!tp@s4o2zk+')
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 ALLOWED_HOSTS = ['*']
 
+# --- APLICACIONES ---
 INSTALLED_APPS = [
-    'cloudinary_storage',
-    'cloudinary',
+    'cloudinary_storage', #
+    'cloudinary',        #
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -19,9 +21,10 @@ INSTALLED_APPS = [
     'hv_app',
 ]
 
+# --- MIDDLEWARE ---
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', #
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -41,13 +44,14 @@ TEMPLATES = [{
             'django.template.context_processors.request',
             'django.contrib.auth.context_processors.auth',
             'django.contrib.messages.context_processors.messages',
-            'django.template.context_processors.media',
+            'django.template.context_processors.media', #
         ],
     },
 }]
 
 WSGI_APPLICATION = 'proyecto_hv.wsgi.application'
 
+# --- BASE DE DATOS ---
 DATABASES = {
     'default': dj_database_url.config(
         default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
@@ -55,15 +59,17 @@ DATABASES = {
     )
 }
 
+# --- INTERNACIONALIZACIÓN ---
 LANGUAGE_CODE = 'es-ec'
 TIME_ZONE = 'America/Guayaquil'
 USE_I18N = True
 USE_TZ = True
 
+# --- ARCHIVOS ESTÁTICOS Y ALMACENAMIENTO ---
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_final')
 
-# --- NUEVA CONFIGURACIÓN OBLIGATORIA PARA DJANGO 6.0 ---
+# COMPATIBILIDAD DJANGO 6.0 + CLOUDINARY
 STORAGES = {
     "default": {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
@@ -73,10 +79,15 @@ STORAGES = {
     },
 }
 
+# Líneas de respaldo para evitar el AttributeError en Render
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
+# --- CREDENCIALES DE CLOUDINARY ---
 CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
-    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
-    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'), #
+    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),     #
+    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'), #
 }
 
 MEDIA_URL = '/media/'
