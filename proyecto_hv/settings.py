9acad8aece8d3d5a6e2cca38e11b3bbdb448955d
joverm12 <joverjesusmoreiramero@gmail.com>
2026-01-28 16:51:23 -0500

    vamos vamos

--- proyecto_hv/settings.py
+++ proyecto_hv/settings.py
@@ -2,16 +2,14 @@ import os
 import dj_database_url
 from pathlib import Path
 
-# --- CONFIGURACIÓN BÁSICA ---
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-abkt2ns#zz0)%=*^%7-n5oihoq+h754!b=g1#fl!tp@s4o2zk+')
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 ALLOWED_HOSTS = ['*']
 
-# --- APLICACIONES ---
 INSTALLED_APPS = [
-    'cloudinary_storage', #
-    'cloudinary',        #
+    'cloudinary_storage',
+    'cloudinary',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -21,10 +19,9 @@ INSTALLED_APPS = [
     'hv_app',
 ]
 
-# --- MIDDLEWARE ---
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware', #
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -44,14 +41,13 @@ TEMPLATES = [{
             'django.template.context_processors.request',
             'django.contrib.auth.context_processors.auth',
             'django.contrib.messages.context_processors.messages',
-            'django.template.context_processors.media', #
+            'django.template.context_processors.media',
         ],
     },
 }]
 
 WSGI_APPLICATION = 'proyecto_hv.wsgi.application'
 
-# --- BASE DE DATOS ---
 DATABASES = {
     'default': dj_database_url.config(
         default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
@@ -59,35 +55,30 @@ DATABASES = {
     )
 }
 
-# --- INTERNACIONALIZACIÓN ---
 LANGUAGE_CODE = 'es-ec'
 TIME_ZONE = 'America/Guayaquil'
 USE_I18N = True
 USE_TZ = True
 
-# --- ARCHIVOS ESTÁTICOS Y ALMACENAMIENTO ---
+# --- ARCHIVOS ESTÁTICOS (Solución al error 404 del Admin) ---
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_final')
 
-# COMPATIBILIDAD DJANGO 6.0 + CLOUDINARY
+# --- CONFIGURACIÓN DE ALMACENAMIENTO DJANGO 6.0 ---
 STORAGES = {
     "default": {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
     "staticfiles": {
-        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
 }
 
-# Líneas de respaldo para evitar el AttributeError en Render
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-
-# --- CREDENCIALES DE CLOUDINARY ---
+# Compatibilidad para la librería de Cloudinary
 CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'), #
-    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),     #
-    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'), #
+    'CLOUD_NAME': 'drhblvng5',
+    'API_KEY': '945383893211668',
+    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
 }
 
 MEDIA_URL = '/media/'
