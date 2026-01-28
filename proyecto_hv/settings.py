55917e2123a5fd05d831bafa8bde02d9f5808602
joverm12 <joverjesusmoreiramero@gmail.com>
2026-01-27 22:17:07 -0500

    Forzando nueva carpeta

--- proyecto_hv/settings.py
+++ proyecto_hv/settings.py
@@ -62,7 +62,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Usamos el almacenamiento base para que Render no se confunda
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_final')
 
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
